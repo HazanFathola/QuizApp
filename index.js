232a9f977487abdbd6f2ce23ce80aeaa3c6a9139
Hassan Mohammed <hazan.fathola@gmail.com>
2026-01-29 14:57:41 +0100

    Build CSS and HTML for Quiz App

---
+++ index.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    question: "Was ist die Hauptstadt von Deutschland?",
+    answers: [
+      {
+        id: "a",
+        text: "München",
+        correct: false,
+      },
+      {
+        id: "b",
+        text: "Berlin",
+        correct: true,
+      },
+      {
+        id: "c",
+        text: "Hannover",
+        correct: false,
+      },
+      {
+        id: "d",
+        text: "Hamburg",
+        correct: false,
+      },
+    ],
+  },
+];
